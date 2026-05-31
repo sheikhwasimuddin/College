3581f276c7efecb541cf6d296e4844c501134601
Sheikh Wasimuddin <161950279+sheikhwasimuddin@users.noreply.github.com>
2026-05-31 22:37:00 +0530

    Add countDigits method to count number of digits

---
+++ CRT/Technical/countDigits.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int countDigits(int n) {
+        if (n == 0) return 1;
+
+        int count = 0;
+        while (n != 0) {
+            count++;
+            n /= 10;
+        }
+        return count;
+    }
+}
