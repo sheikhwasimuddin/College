2b13611dc7baaabff8d1e5595ca48dc4ecb296eb
Sheikh Wasimuddin <161950279+sheikhwasimuddin@users.noreply.github.com>
2026-05-31 22:37:36 +0530

    Add Fibonacci sequence implementation in Java

---
+++ CRT/Technical/fibonacci.java
@@ -0,0 +1,13 @@
+class Solution {
+    public void fibonacci(int n) {
+        int a = 0, b = 1;
+
+        for (int i = 1; i <= n; i++) {
+            System.out.print(a + " ");
+
+            int c = a + b;
+            a = b;
+            b = c;
+        }
+    }
+}
